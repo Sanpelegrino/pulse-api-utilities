6c23271accb0be14a3c49193acfe54a4b2e615ab
Sanpelegrino <andrew.hill@salesforce.com>
2026-02-05 21:33:51 -0800

    Heroku deployment config

--- app.py
+++ app.py
@@ -4497,4 +4497,5 @@ def zero_follower_metrics():
 
 if __name__ == '__main__':
     # Run the Flask development server
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    port = int(os.environ.get('PORT', '3000'))
+    app.run(debug=True, host='0.0.0.0', port=port)
